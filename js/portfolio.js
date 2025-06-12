1e500599aadb710b3bb0ae97654f573d7d4cd4a7
priyan1995 <priyan.abewickrama@ideahub.lk>
2025-06-12 22:03:53 +0530

    projects update with json

---
+++ js/portfolio.js
@@ -0,0 +1,355 @@
+
+const data = {
+    "projects": [
+        {
+            "title": "Global Sea Foods",
+            "tech": "WordPress",
+            "link": "https://gsffish.com/",
+            "image": "img/projects/29.jpg",
+            "companyCredit": ""
+        },
+        {
+            "title": "Graceland Tea Factory",
+            "tech": "WordPress",
+            "link": "https://gracelandteafactory.com/",
+            "image": "img/projects/17.jpg",
+            "companyCredit": "By BflyDigital"
+        },
+        {
+            "title": "Kuwait Farmers Market",
+            "tech": "WordPress",
+            "link": "https://kuwaitfarmersmarket.com/",
+            "image": "img/projects/kfm.jpg",
+            "companyCredit": ""
+        },
+        {
+            "title": "Roo Leo",
+            "tech": "WordPress",
+            "link": "http://rooleo.com/",
+            "image": "img/projects/28.jpg",
+            "companyCredit": ""
+        },
+        {
+            "title": "I love Coco",
+            "tech": "WordPress",
+            "link": "https://ilovecoco.co/",
+            "image": "img/projects/18.jpg",
+            "companyCredit": "By Lizard Advertising"
+        },
+        {
+            "title": "Epsicon Traffic Management",
+            "tech": "WordPress",
+            "link": "https://epsicontraffic.com.au/",
+            "image": "img/projects/1.jpg",
+            "companyCredit": ""
+        },
+        {
+            "title": "Guardian Acuity",
+            "tech": "UI / Front-End",
+            "link": "https://www.guardianacuity.com/",
+            "image": "img/projects/30.jpg",
+            "companyCredit": "By Ogilvy Digital"
+        },
+         {
+            "title": "Sunquick Recipe",
+            "tech": "Wordpress",
+            "link": "https://sunquickrecipesrilanka.com/",
+            "image": "img/projects/31.jpg",
+            "companyCredit": "By Ogilvy Digital"
+        },
+          {
+            "title": "AOY",
+            "tech": "UI / Front-End",
+            "link": "https://priyan.ogilvydigital.tech/AOY/",
+            "image": "img/projects/32.jpg",
+            "companyCredit": "By Ogilvy Digital"
+        },
+            {
+            "title": "Spiralation",
+            "tech": "Wordpress",
+            "link": "https://spiralation.com/",
+            "image": "img/projects/19.jpg",
+            "companyCredit": "By Ogilvy Digital"
+        }
+    ]
+};
+
+
+
+
+
+
+
+
+
+{/*
+
+
+// 	
+// 
+// 
+// 		<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://ferentino.com/" target="_blank">
+// 					<img src="img/projects/26.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Ferentino</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Ogilvy Digital</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://priyan.ogilvydigital.tech/lanwa/" target="_blank">
+// 					<img src="img/projects/27.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Lanwa</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Ogilvy Digital</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://bhagyahettiarachchi.com/" target="_blank">
+// 					<img src="img/projects/20.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Bhagya Hettiarachchi</h4>
+// 						<p>Wordpress</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="https://priyan1995.github.io/js-parallax/" target="_blank">
+// 					<img src="img/projects/25.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Practise Task</h4>
+// 						<p>HTML/CSS/JS</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://aimun.net/" target="_blank">
+// 					<img src="img/projects/21.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>AIMUN</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Orpheus</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://boroughmarkets.com.au/" target="_blank">
+// 					<img src="img/projects/22.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Borough Market</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Dream Squad</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://hyperspacehub.com/" target="_blank">
+// 					<img src="img/projects/23.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Hyperspace</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Dream Squad</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="https://madhalankatours.com/" target="_blank">
+// 					<img src="img/projects/2.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>MadhaLanka Tours</h4>
+// 						<p>Wordpress</p>
+// 					</div>
+// 				</a>
+// 			</div>
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://bflydigital.com/" target="_blank">
+// 					<img src="img/projects/3.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>BflyDigital</h4>
+// 						<p>Wordpress</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://www.sanatiospa.lk/" target="_blank">
+// 					<img src="img/projects/4.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Sanatio Spa</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Idea Hub</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="http://wrmarcdesigns.com/" target="_blank">
+// 					<img src="img/projects/5.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>WRM Architechture</h4>
+// 						<p>Wordpress</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="http://kodafi.com/dev/" target="_blank">
+// 					<img src="img/projects/6.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Ko-Da-Fi</h4>
+// 						<p>HTML</p>
+// 					</div>
+// 				</a>
+// 			</div>
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="http://lankanewsweek.com/" target="_blank">
+// 					<img src="img/projects/7.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Lanka Newsweek</h4>
+// 						<p>PHP</p>
+// 						<p class="pd-company-credit">By Ozonedesk</p>
+// 					</div>
+// 				</a>
+// 			</div>
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="https://www.creationsmaster.com/" target="_blank">
+// 					<img src="img/projects/8.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Creation Master Builders</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Web lankan</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="https://hyperjet.lk/" target="_blank">
+// 					<img src="img/projects/24.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Hyperjet</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Web lankan</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="http://www.camlolanka.com/" target="_blank">
+// 					<img src="img/projects/11.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Camlo Lanka Website</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Web lankan</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="http://www.tsaragreentea.com/" target="_blank">
+// 					<img src="img/projects/13.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Tsara Website</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Web lankan</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://www.nsbm.ac.lk/" target="_blank">
+// 					<img src="img/projects/16.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>NSBM - Responsive/few pages developed</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Web lankan</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="http://www.nestacademy.lk/" target="_blank">
+// 					<img src="img/projects/9.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Nest Academy Website</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Web lankan</p>
+// 					</div>
+// 				</a>
+// 			</div>
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="http://www.cocofresh.lk/" target="_blank">
+// 					<img src="img/projects/10.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Coco Fresh Website</h4>
+// 						<p>Wordpress</p>
+// 						<p class="pd-company-credit">By Web lankan</p>
+// 					</div>
+// 				</a>
+// 			</div>
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="https://www.ownetutor.lk/" target="_blank">
+// 					<img src="img/projects/12.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>Own E-Tutor</h4>
+// 						<p>HTML/Css</p>
+// 						<p class="pd-company-credit">By Web lankan</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item" href="http://piyawardenaassociates.com/esessecretaries.lk" target="_blank">
+// 					<img src="img/projects/15.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>ESES SECRETARIES</h4>
+// 						<p>HTML/CSS</p>
+// 						<p class="pd-company-credit">By bflydigital</p>
+// 					</div>
+// 				</a>
+// 			</div>
+
+// 			<div class="col-lg-4 col-xl-3 col-sm-6">
+// 				<a class="gallery-item " href="http://bflydigital.com/ISF/" target="_blank">
+// 					<img src="img/projects/14.jpg" alt="">
+// 					<div class="gi-text">
+// 						<h4>ISF Website</h4>
+// 						<p>HTML / CSS</p>
+// 						<p class="pd-company-credit">By BflyDigital</p>
+// 					</div>
+// 				</a>
+// 			</div>
+// 		</div>
+
+ */}
+
+
+const container = document.getElementById('projects-container');
+data.projects.forEach(project => {
+    const html = `
+      <div class="col-lg-4 col-xl-3 col-sm-6">
+        <a class="gallery-item" href="${project?.link}" target="_blank">
+          <img src="${project?.image}" alt="${project?.title}">
+          <div class="gi-text">
+            <h4>${project?.title}</h4>
+            <p>${project?.tech}</p>
+				<p class="pd-company-credit">${project?.companyCredit}</p>
+          </div>
+        </a>
+      </div>
+    `;
+    container.insertAdjacentHTML('beforeend', html);
+});
